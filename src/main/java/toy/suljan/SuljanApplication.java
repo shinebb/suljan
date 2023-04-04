928a693e7a4de449c1b217c0bd13fa9f8c3a4b7c
Dohee Kim <ododalrio@gmail.com>
2023-04-05 00:02:03 +0900

    230405 00:01 셋팅진행중

---
+++ src/main/java/toy/suljan/SuljanApplication.java
@@ -0,0 +1,13 @@
+package toy.suljan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuljanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuljanApplication.class, args);
+	}
+
+}
